7068162b624aaba59db5634599bf31fe825c88b6
orlando  arias <orlandoarias@orlandos-MacBook-Pro.local>
2020-04-07 11:11:02 -0500

    Updates

---
+++ precache-manifest.d64797cc0f91583d0d20a55fe480f841.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f538e4d7381f53c975fd0fa49352a22",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bd34664c20994143844f",
+    "url": "/monsters-rolodex/static/css/main.930b0d69.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd34664c20994143844f",
+    "url": "/monsters-rolodex/static/js/main.469373d1.chunk.js"
+  },
+  {
+    "revision": "83fef09b6b4b91e8e383",
+    "url": "/monsters-rolodex/static/js/runtime-main.6036d2a0.js"
+  }
+]);
